d3c0b4b7878a0bcb041acd8c78964343a84e7d6a
Acedia26 <user@email.com>
2022-08-07 17:05:50 +0700

    update main js

--- JS/Main.js
+++ JS/Main.js
@@ -1,3 +1,15 @@
+console.log("Hi apa kabar!");
+
+var nama = prompt("Masukan Nama:");
+alert(nama);
+
+var tes = confirm("yakin mau lihat ??");
+if (tes === true) {
+  alert("Welcome");
+} else {
+  alert("yahhhh");
+}
+
 var tombolMenu = document.getElementsByClassName("tombol-menu")[0];
 var menu = document.getElementsByClassName("menu")[0];
 
